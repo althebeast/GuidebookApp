0afba89429340585f3372dc591796bc655d40c99
Alperen <alperensarisan@icloud.com>
2024-02-05 01:14:43 -0400

    Views are done

---
+++ GuidebookApp/CityCard.swift
@@ -0,0 +1,18 @@
+//
+//  CityCard.swift
+//  GuidebookApp
+//
+//  Created by Alperen Sarışan on 5.02.2024.
+//
+
+import SwiftUI
+
+struct CityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityCard()
+}
