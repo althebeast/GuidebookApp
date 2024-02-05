92eaff70ffc45a8a40eaa160bfc31b336ac16168
Alperen <alperensarisan@icloud.com>
2024-02-04 23:53:56 -0400

    Initial Commit

---
+++ GuidebookApp/GuidebookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookAppApp.swift
+//  GuidebookApp
+//
+//  Created by Alperen Sarışan on 4.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
